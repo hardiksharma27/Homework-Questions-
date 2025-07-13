f95680f707658c39c731521845ae083d9d5eafe4
hardiksharma27 <hardiksharma13026q@gmail.com>
2025-07-13 11:42:42 +0530

    Update day 12 question.py

--- day 12 question.py
+++ day 12 question.py
@@ -6,3 +6,11 @@ with open("files.txt", "w") as file:
 
 with open("files.txt", "r") as file:
     print(file.read())
+
+OUTPUT ---
+
+PS C:\next yug classes> & C:/Users/DELL/AppData/Local/Programs/Python/Python313/python.exe "c:/next yug classes/day 12 question.py"
+Enter your name: hardik
+Enter your age: 18
+Name: hardik
+Age: 18
